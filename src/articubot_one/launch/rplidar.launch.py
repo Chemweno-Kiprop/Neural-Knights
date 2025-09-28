d0af9e8231034be100a10da072d01a3b96281cdc
Collins Kiprop Chemweno <collinskiprop59@gmail.com>
2025-09-29 02:55:28 +0300

    Refactor RPLIDAR launch configuration to use rplidar_ros package and update parameters

--- src/articubot_one/launch/rplidar.launch.py
+++ src/articubot_one/launch/rplidar.launch.py
@@ -3,19 +3,18 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
+
     return LaunchDescription([
+
         Node(
-            package='sllidar_ros2',
-            executable='sllidar_node',
-            name='sllidar_node',
+            package='rplidar_ros',
+            executable='rplidar_composition',
             output='screen',
             parameters=[{
                 'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-port0',
                 'frame_id': 'laser_frame',
-                'channel_type': 'serial',
-                'serial_baudrate': 115200,  # use 256000 if your RPLIDAR model requires it
-                'inverted': False,
-                'angle_compensate': True
+                'angle_compensate': True,
+                'scan_mode': 'Standard'
             }]
         )
     ])
